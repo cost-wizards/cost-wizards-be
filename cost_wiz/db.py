a34b90700d98df0e7b9a4844aa18a61edde0906b
Jenish Shrestha <jenishshrestha@lftechnology.com>
2024-04-06 03:49:18 +0545

    add migration for creating tables

--- cost_wiz/db.py
+++ cost_wiz/db.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Boolean, Column, Integer, String, create_engine
+from sqlalchemy import (Boolean, Column, DateTime, Float, ForeignKey, Integer,
+                        String, create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from cost_wiz.config.settings import env
 
-print(env.get_db_url())
 engine = create_engine(env.get_db_url())
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -12,9 +12,67 @@ Base = declarative_base()
 
 
 class Account(Base):
-    __tablename__ = "users"
+    __tablename__ = "account"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    name = Column(String)
+    access_key = Column(String)
+    secret_key = Column(String)
+    region = Column(String)
+
+    has_account_setup = Column(Boolean)
+
+
+class Instance(Base):
+    __tablename__ = "instance"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    instance_id = Column(String)
+    instance_type = Column(String)
+    cpu = Column(String)
+    ram = Column(String)
+    status = Column(String)
+    hourly_rate = Column(Float)
+
+    account_id = Column(Integer, ForeignKey("account.id"))
+
+
+class InstanceStat(Base):
+
+    __tablename__ = "instance_stats"
+
+    id = Column(Integer, primary_key=True)
+    instance_id = Column(Integer, ForeignKey("instance.id"))
+
+    mem_used_percent = Column(Float)
+    cpu_usage_iowait = Column(Float)
+    cpu_usage_idle = Column(Float)
+    cpu_usage_system = Column(Float)
+    diskio_reads = Column(Float)
+    cpu_usage_user = Column(Float)
+    disk_used_percent = Column(Float)
+    swap_used_percent = Column(Float)
+
+    timestamp = Column(DateTime)
+
+
+class Recommendation(Base):
+
+    __tablename__ = "recommendation"
+
+    id = Column(Integer, primary_key=True)
+
+    instance_id = Column(Integer, ForeignKey("instance.id"))
+
+    sug_1_instance_type = Column(String)
+    sug_1_reason = Column(String)
+    sug_1_cost_per_hour = Column(Float)
+    sug_1_diff_cost_per_hour = Column(Float)
+
+    sug_2_instance_type = Column(String)
+    sug_2_reason = Column(String)
+    sug_2_cost_per_hour = Column(Float)
+    sug_2_diff_cost_per_hour = Column(Float)
+
+    account_id = Column(Integer, ForeignKey("account.id"))
